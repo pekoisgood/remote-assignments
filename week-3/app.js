7804d5b6fa981a881ada2ce2915df776bb4f1813
pekoisgood <peko.claire@gmail.com>
2023-04-11 17:07:51 +0800

    upload week-3 assignment

---
+++ week-3/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>')
+})
+
+app.get('/getData', (req, res) => {
+    const { number } = req.query;
+    if (!number){
+        return res.send('<h1>Lack of Parameter</h1>')
+    } else if ( +number >= 0 ) {
+        let initNumber = 0;
+        for (let i =0; i < +number+1; i++){
+            initNumber += i;
+        }
+        return res.send(`<h1>${initNumber}</h1>`)
+    }
+    return res.send('<h1>Wrong Parameter</h1>')
+})
+
+app.listen(3000,() => console.log('The server is listening on localhost:3000...'));
