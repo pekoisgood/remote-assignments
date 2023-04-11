dd44324b3826399dd437e433b24035224521da2e
pekoisgood <peko.claire@gmail.com>
2023-04-11 17:18:25 +0800

    update port at app

--- week-3/app.js
+++ week-3/app.js
@@ -21,4 +21,5 @@ app.get('/getData', (req, res) => {
     return res.send('<h1>Wrong Parameter</h1>')
 })
 
-app.listen(3000,() => console.log('The server is listening on localhost:3000...'));
+const port = process.env.PORT || 3000;
+app.listen( port, () => console.log('The server is listening on localhost:3000...'));
