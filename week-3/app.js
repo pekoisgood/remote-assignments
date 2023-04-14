7c2dbcf8be9603628fb53d1165c0971622f9714f
pekoisgood <peko.claire@gmail.com>
2023-04-14 23:09:45 +0800

    update index.html and coding style

--- week-3/app.js
+++ week-3/app.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cookieParser());
 
-const router = require('./routes');
+const router = require("./routes");
 app.use(router);
 
 const port = process.env.PORT || 3000;
-app.listen( port, () => console.log('The server is listening on localhost:3000...'));
+app.listen(port, () =>
+  console.log("The server is listening on localhost:3000...")
+);
--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>')
-})
+router.get("/", (req, res) => {
+  res.send("<h1>Hello world!</h1>");
+});
 
-router.get('/getData', (req, res) => {
-    const { number } = req.query;
-    if (!number){
-        return res.json({ message: "Lack of Parameter" })
-    } else if ( +number >= 0 && Number.isInteger(+number) ) {
-        let initNumber = 0;
-        for (let i =0; i < +number+1; i++){
-            initNumber += i;
-        }
-        return res.json({ message: `${initNumber}` })
+router.get("/getData", (req, res) => {
+  const { number } = req.query;
+  if (!number) {
+    return res.json({ message: "Lack of Parameter" });
+  } else if (+number >= 0 && Number.isInteger(+number)) {
+    let initNumber = 0;
+    for (let i = 0; i < +number + 1; i++) {
+      initNumber += i;
     }
-    return res.json({message: "Wrong Parameter"})
-})
+    return res.json({ message: `${initNumber}` });
+  }
+  return res.json({ message: "Wrong Parameter" });
+});
 
-router.get('/myName', (req, res) => {
-    const { username}  = req.cookies;
-    if (!username) {
-        return res.send(`
+router.get("/myName", (req, res) => {
+  const { username } = req.cookies;
+  if (!username) {
+    return res.send(`
         <form action="/trackName" method="get">
             <input type="text" name="name"></input>
             <button type="submit">Submit</button>
         </form>
-        `);    
-    } else {
-       return res.send(`${username}`);
-    }
-})
+        `);
+  } else {
+    return res.send(`${username}`);
+  }
+});
 
-router.get('/trackName', (req, res) => {
-    const { name } = req.query;
-    res.cookie('username', name);
-    res.redirect('/myName');
-})
+router.get("/trackName", (req, res) => {
+  const { name } = req.query;
+  res.cookie("username", name);
+  res.redirect("/myName");
+});
 
 module.exports = router;
